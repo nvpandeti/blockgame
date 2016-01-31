d70863a3cef7819a1a19cab1c03c0eb555afbf67
nvpandeti <nikhil.pandeti@gmail.com>
2016-01-31 10:19:17 -0600

    Just saying hi :)
    Added the cube class
    can now draw multiple shapes

--- src/snake3D/perspective.py
+++ src/snake3D/perspective.py
@@ -21,6 +21,15 @@ class Face:
         
         self.center = (xSum/4, ySum/4, zSum/4)
         print(self.center)
+class Cube:
+    def __init__(self, x, y, z, length, width, height):
+        self.cube = ((x+length,y+width,z+height), (x+length,y+width,z), (x+length,y,z+height), (x+length,y,z), (x,y+width,z+height), (x,y+width,z), (x,y,z+height), (x,y,z))
+        self.faces = [Face(self.cube[0],self.cube[1],self.cube[3],self.cube[2],0),
+                      Face(self.cube[0],self.cube[1],self.cube[5],self.cube[4],1),
+                      Face(self.cube[4],self.cube[5],self.cube[7],self.cube[6],2),
+                      Face(self.cube[6],self.cube[7],self.cube[3],self.cube[2],3),
+                      Face(self.cube[6],self.cube[4],self.cube[0],self.cube[2],4),
+                      Face(self.cube[7],self.cube[5],self.cube[1],self.cube[3],5)]
 global size;
 size = [900,550]
 speed = 20   #float(input("How fast do you want to go?(FPS)"))
@@ -34,7 +43,8 @@ keys = pygame.key.get_pressed()
 screen.fill([0,0,0])
 posH = 0
 posZ = 45
-r = 10
+r = 20
+"""
 cube = ((1,1,1), (1,1,-1), (1,-1,1), (1,-1,-1), (-1,1,1), (-1,1,-1), (-1,-1,1), (-1,-1,-1))
 faces = [Face(cube[0],cube[1],cube[3],cube[2],0),
          Face(cube[0],cube[1],cube[5],cube[4],1),
@@ -42,6 +52,23 @@ faces = [Face(cube[0],cube[1],cube[3],cube[2],0),
          Face(cube[6],cube[7],cube[3],cube[2],3),
          Face(cube[6],cube[4],cube[0],cube[2],4),
          Face(cube[7],cube[5],cube[1],cube[3],5)]
+"""
+shapes = []
+shapes.append(Cube(-1,-1,-1,2,2,2))
+shapes.append(Cube(-3,-1,-1,2,2,2))
+shapes.append(Cube(1,-1,-1,2,2,2))
+shapes.append(Cube(3,-1,-1,2,2,2))
+shapes.append(Cube(3,-1,1,2,2,2))
+shapes.append(Cube(3,-1,3,2,2,2))
+shapes.append(Cube(3,-1,-3,2,2,2))
+shapes.append(Cube(3,-1,-5,2,2,2))
+shapes.append(Cube(-3,-1,1,2,2,2))
+shapes.append(Cube(-3,-1,3,2,2,2))
+shapes.append(Cube(-3,-1,-3,2,2,2))
+shapes.append(Cube(-3,-1,-5,2,2,2))
+shapes.append(Cube(7,-1,1,2,2,2))
+shapes.append(Cube(7,-1,-3,2,2,2))
+shapes.append(Cube(7,-1,-5,2,2,2))
 
 while x == False:
     for event in pygame.event.get():
@@ -68,18 +95,25 @@ while x == False:
     realY = r * math.sin(math.radians(posH)) * math.cos(math.radians(posZ))
     realZ = r * math.sin(math.radians(posZ))
     
+    faces = []
+    for s in shapes:
+        for face in s.faces:
+            faces.append(face)
+    
+    
     for i in range(faces.__len__()-1):
-        sorted = False
+        sorted = True
         for j in range(faces.__len__()-1-i):
             if(math.sqrt((realX - faces[j].center[0])**2 + (realY - faces[j].center[1])**2 + (realZ - faces[j].center[2])**2)
                < math.sqrt((realX - faces[j+1].center[0])**2 + (realY - faces[j+1].center[1])**2 + (realZ - faces[j+1].center[2])**2)):
                 temp = faces[j]
                 faces[j] = faces[j+1]
                 faces[j+1] = temp
-                sorted = True
+                sorted = False
         if(sorted):
             break
         
+    
     for face in faces:
         
         angleD = []
@@ -95,12 +129,14 @@ while x == False:
             angleD.append( math.degrees( math.acos( v1norm[0] * v2norm[0] + v1norm[1] * v2norm[1] + v1norm[2] * v2norm[2] ) ) )
             """
             
-            angleD.append( math.degrees( math.acos( ( (0 - realX)*(corner[0] - realX) + (0 - realY)*(corner[1] - realY) + (0 - realZ)*(corner[2] - realZ) ) / ( math.sqrt( math.pow( 0 - realX, 2) + math.pow( 0 - realY, 2) + math.pow( 0 - realZ, 2) ) *  math.sqrt( math.pow( corner[0] - realX, 2) + math.pow( corner[1] - realY, 2) + math.pow( corner[2] - realZ, 2) ) ) ) ) )
+            angleD.append( math.degrees( math.acos( ( (0 - realX)*(corner[0] - realX) + (0 - realY)*(corner[1] - realY) + (0 - realZ)*(corner[2] - realZ) ) 
+                                                    / ( math.sqrt( ( 0 - realX)**2 + ( 0 - realY)**2 + ( 0 - realZ)**2 ) *  math.sqrt( ( corner[0] - realX)**2 + ( corner[1] - realY)**2 + ( corner[2] - realZ)**2 ) ) ) ) )
             
             t = - ( (0 - realX)*(realX - corner[0]) + (0 - realY)*(realY - corner[1]) + (0 - realZ)*(realZ - corner[2]) ) / ( (0 - realX)**2 + (0 - realY)**2 + (0 - realZ)**2 )
             vertex = [realX + (0 - realX)*t, realY + (0 - realY)*t, realZ + (0 - realZ)*t]
             perpendicular = [vertex[0] - (0 - realY), vertex[1] + (0 - realX), vertex[2]]
-            R = math.acos( ( (perpendicular[0] - vertex[0])*(corner[0] - vertex[0]) + (perpendicular[1] - vertex[1])*(corner[1] - vertex[1]) + (perpendicular[2] - vertex[2])*(corner[2] - vertex[2]) ) / ( math.sqrt( math.pow( perpendicular[0] - vertex[0], 2) + math.pow( perpendicular[1] - vertex[1], 2) + math.pow( perpendicular[2] - vertex[2], 2) ) *  math.sqrt( math.pow( corner[0] - vertex[0], 2) + math.pow( corner[1] - vertex[1], 2) + math.pow( corner[2] - vertex[2], 2) ) ) )
+            R = math.acos( ( (perpendicular[0] - vertex[0])*(corner[0] - vertex[0]) + (perpendicular[1] - vertex[1])*(corner[1] - vertex[1]) + (perpendicular[2] - vertex[2])*(corner[2] - vertex[2]) ) 
+                           / ( math.sqrt( ( perpendicular[0] - vertex[0])**2 + ( perpendicular[1] - vertex[1])**2 + ( perpendicular[2] - vertex[2])**2 ) *  math.sqrt( ( corner[0] - vertex[0])**2 + ( corner[1] - vertex[1])**2 + ( corner[2] - vertex[2])**2 ) ) )
             if(corner[2]<perpendicular[2]):
                 angleR.append(2*math.pi - R)
             else:
